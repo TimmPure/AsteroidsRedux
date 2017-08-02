b676c4f37affe72355e882bef12bc002ddbaa55c
TimmPure <T.j.m.voskuil@gmail.com>
2017-08-02 22:27:16 +0200

    Background parallax added
    Doesn't look right at all,
    because the reference frame (camera) does not move with player.
    Committing it anyways for future reference/inspiration.

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Parallax : MonoBehaviour {
+
+    private MeshRenderer background;
+    private GameObject player;
+
+    private float parallax = 10f;
+
+	void Start () {
+        background = GetComponent<MeshRenderer>();
+
+        player = GameObject.Find("Player");
+        if (!player) { Debug.LogError("No Player found for parallax"); }
+    }
+	
+	void Update () {
+        background.material.mainTextureOffset = Vector2.Lerp(background.material.mainTextureOffset, player.transform.position / parallax, 0.8f);
+	}
+}
