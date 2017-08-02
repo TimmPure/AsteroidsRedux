6d723809ee0ebf04bb9d671083703ff3cedaeae0
NPetterson <n.j.petterson@tue.nl>
2017-08-02 21:11:15 +0200

    Reworked player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour {
 
-    private float maxSpeed = 5f;                                                        //Speed in world units per second
+    private float maxForce = 500f;
+    private float maxSpeed = 7f;                                                        //Speed in world units per second
     private float rotationSpeed = 180f;                                                 //Rotation in Euler degrees per second
-	
-	void Update () {
+    private Rigidbody2D rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
         //Move
-        Vector2 yDelta = Vector2.up * Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;    //Vector2.up is shorthand for (0,1). Using that to prevent having to create new Vector2 in update
+        Vector2 yDelta = Vector2.up * Input.GetAxisRaw("Vertical") * maxForce * Time.deltaTime;    //Vector2.up is shorthand for (0,1). Using that to prevent having to create new Vector2 in update
 
         //Rotate
         Quaternion rotation = transform.rotation;
@@ -19,6 +26,14 @@ public class PlayerMovement : MonoBehaviour {
 
         //Update transform based on rotation and movement
         transform.rotation = rotation;
-        transform.position += rotation * yDelta;
+
+        rb.AddForce(rotation * yDelta);
+        
 	}
+
+    private void FixedUpdate() {
+        rb.velocity = rb.velocity.normalized* Mathf.Min(rb.velocity.magnitude, maxSpeed);
+        
+    }
+
 }
