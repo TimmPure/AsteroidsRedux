a9065616706b78767a629ee917695fa070cb15ae
Ramonnn <ramon.eliens@gmail.com>
2017-08-02 15:46:39 +0200

    Asteroid object created, close #4

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class Asteroid : MonoBehaviour {
+
+    public float initialVelocity = 100f;
+    private Transform parent;
+
+    private Vector2 randomVector;
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        randomVector = new Vector2(Random.Range(-1f,1f), Random.Range(-1f, 1f)).normalized;
+        rb.AddForce(randomVector * initialVelocity);
+        parent = GameObject.Find("Asteroids").transform;
+        if (!parent) { Debug.LogError("No Asteroid parent object found on " + name); }
+
+        transform.parent = parent;
+	}
+}
